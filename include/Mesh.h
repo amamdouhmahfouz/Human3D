7fdb69597ff0094ecdded81f13d2f83d0d38eba7
amamdouhmahfouz <abdelrahman.abdelghany99@eng-st.cu.edu.eg>
2022-05-18 05:25:23 +0200

    added get point at index function in Mesh

--- include/Mesh.h
+++ include/Mesh.h
@@ -2,7 +2,11 @@
 #define MESH_H
 
 #include <vector>
+#include <cmath>
 #include "Vertex.h"
+#include "Point.h"
+#include "TriangleCell.h"
+//#include <glm/glm.hpp>
 
 class Mesh {
 private:  
@@ -10,8 +14,29 @@ private:
 public:  
     std::vector<Vertex> vertices;
     std::vector<unsigned int> indices;
+    std::vector<glm::vec3> faces;
+
+    std::vector<Point<glm::vec3>> points;
+    std::vector<unsigned int> pointIds;
+    std::vector<TriangleCell> triangleCells;
+    std::vector<Point<glm::vec3>> normals;
+
+    Mesh();
     Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices);
+    Mesh(std::vector<Point<glm::vec3>> points);
+    Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<glm::vec3> faces);
     ~Mesh();
+
+    void setMesh(std::vector<Point<glm::vec3>> points, std::vector<glm::vec3> faces);
+    void setMesh(std::vector<Point<glm::vec3>> points);
+    Point<glm::vec3> getClosestPoint(glm::vec3 point); // should return a Point instead
+    glm::vec3 getCenterOfMass() const;
+    float getScale() const;
+    void deTranslate(); // makes the mean point of the mesh zeros
+    void deScale(); // makes the scale of the mesh = 1
+
+    Point<glm::vec3> getPointAtIndex(unsigned int pointIndex);
+
 };
 
 #endif //MESH_H
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -1,10 +1,80 @@
 #include "Mesh.h"
 
+Mesh::Mesh() {
+  
+}
+
 Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices) {
     this->vertices = vertices;
     this->indices = indices;
 }
 
+Mesh::Mesh(std::vector<Point<glm::vec3>> points) {
+    this->points = points;
+}
+
+Mesh::Mesh(std::vector<Vertex> vertices, std::vector<unsigned int> indices, std::vector<glm::vec3> faces) {
+    this->vertices = vertices;
+    this->indices = indices;
+    this->faces = faces;
+}
+
 Mesh::~Mesh() {
 
+}
+
+void Mesh::setMesh(std::vector<Point<glm::vec3>> points, std::vector<glm::vec3> faces) {
+    this->points = points;
+    this->faces = faces;
+}
+
+void Mesh::setMesh(std::vector<Point<glm::vec3>> points) {
+    this->points = points;
+}
+
+
+// glm::vec3 Mesh::getClosestPoint(glm::vec3 point) {
+
+// }
+
+glm::vec3 Mesh::getCenterOfMass() const {
+    glm::vec3 center(0.0f,0.0f,0.0f);
+    
+    for (const auto& point : points) {
+        center += point.position;
+    }
+    
+    center /= float(points.size());
+    return center;
+}
+
+float Mesh::getScale() const {
+    float scale = 0.0;
+    for (int i = 0; i < points.size(); i++) {
+        scale += (points[i].position.x*points[i].position.x +points[i].position.y*points[i].position.y + points[i].position.z*points[i].position.z);
+    }
+    scale = sqrt(scale/float(points.size()));
+    return scale;
+}
+
+void Mesh::deTranslate() {
+    glm::vec3 center = getCenterOfMass();
+
+    for (int i = 0; i < points.size(); i++) {
+        points[i].position -= center;
+    }
+}
+
+void Mesh::deScale() {
+    float scale = getScale();
+    
+    for (int i = 0; i < points.size(); i++) {
+        points[i].position /= scale;
+    }
+}
+
+Point<glm::vec3> Mesh::getPointAtIndex(unsigned int pointIndex) {
+    for (int i = 0; i < points.size(); i++) 
+        if (points[i].index == pointIndex)
+            return points[i];
 }
